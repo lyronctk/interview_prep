715dad003011059ca40c2b0a4b59772fda59db88
Lyron Co Ting Keh <lyronctk@gmail.com>
2019-09-30 17:51:46 -0700

    intitialize repo

---
+++ working/general.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+// clang++ -std=c++11 -stdlib=libc++ general.cpp
+// ./a.out
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    setbuf(stdout, NULL);
+    // freopen("in.txt", "r", stdin);
+
+    cout << "HELLO WORLD" << endl;
+}
